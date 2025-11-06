7cdc76d525bf75e04ffd60785db55afb7e5ab69a
Kojo <kwadwoarhin@hotmail.com>
2025-11-06 01:32:59 -0500

    feat: fix fallback

--- server/index.js
+++ server/index.js
@@ -91,7 +91,7 @@ app.post('/parsereceipt', async (req, res) => {
 });
 
 app.use(express.static(path.join(__dirname, "../dist")));
-app.get("*", (req, res) => {
+app.get('/{*any}', (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
